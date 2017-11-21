27adcd42779ee1a883ccf659eee9dead492d23e1
Ashley Jordan <ajordanium@gmail.com>
2017-11-20 17:39:30 -0800

    made pictures do a thing

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
+'use strict';
+
 var allProducts = [];
 
 var productNames = ['bag', 'boots', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb',
-'water-can', 'wine-glass'];
+  'water-can', 'wine-glass'];
 
-// Longest line of code ever!!!
+// You just had to make us use this many pictures...
 
 
 function Product(name, path) {
   this.name = name,
   this.path = path,
-  this.clicked = 0,
+  this.clicked = 0;
   this.viewed = 0;
   allProducts.push(this);
-
-  // TODO: Build your constructor and necessary properties.
 }
+// TODO: Build your constructor and necessary properties.
+
+for(var x = 0; x < productNames.length; x++) {
+  new Product(productNames[x], 'img/' + productNames[x] + '.jpg');
+}
+
+console.log('product names:', productNames);
+
+
 
 // TODO: Don't forget to build your objects. How can you do this withough having to write 14 lines of `new Product(., ., .)`?
 
@@ -22,13 +31,26 @@ var productRank = {
   // TODO: All the properties of the object! What do you think you need? Try to write one piece at a time and make sure it does what you want before writing a little more.
   // NOTE: A-C-P reminder... Make very intentional and iterative changes to your code, and then A-C-P.
 
-  getRandomIndex: function() {
-    // TODO: Hmm... what's going to happen here?
+  getRandomIndex: function(max) {
+    return Math.round(Math.random() * (max - 0)) + 0;
   },
 
   displayImages: function() {
-    // TODO: Hmm... what's going to happen here?
+    var picture =  document.getElementsByClassName('threeImg');
+    var duplicate = [-1, -1, -1];
+
+    while(duplicate[0] === duplicate[1] || duplicate[0] === duplicate[2] || duplicate[1] === duplicate[2]) {
+      duplicate = [productRank.getRandomIndex(allProducts.length), productRank.getRandomIndex(allProducts.length), productRank.getRandomIndex(allProducts.length)];
+    }
+
+    for(var i = 0; i < 3; i++) {
+      picture[i].src = allProducts[duplicate[i]].path;
+      allProducts[duplicate[i]].timesShown++;
+    }
   },
+
+
+
 
   tallyClicks: function(elementId) {
     // TODO: Hmm... what's going to happen here?
@@ -44,7 +66,8 @@ var productRank = {
 
   onClick: function() {
     // TODO: Hmm... what's going to happen here?
+  },
 };
 
-productRank.imageEls.addEventListener('click', productRank.onClick);
+// productRank.imageEls.addEventListener('click', productRank.onClick);
 productRank.displayImages();
