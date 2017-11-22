26e9c75640ae3f3a404222443a23ae2fc9d5a23b
Ashley Jordan <ajordanium@gmail.com>
2017-11-22 14:55:44 -0800

    added chart and made it 25 clicks

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 'use strict';
-
 var allProducts = [];
-
-var productNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'sweepers', 'tauntaun', 'unicorn', 'usb',
-  'water-can', 'wine-glass'];
+var productNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
 function Product(name) {
   this.name = name;
   this.path = 'img/' + this.name + '.jpg';
+  this.clicked = 0;
   this.votes = 0;
-  // this.timesShown = 0;
   allProducts.push(this);
 }
 
 (function() {
-  for(var i in productNames) {
+  for(var i in productNames){
     new Product(productNames[i]);
   }
 })();
@@ -56,7 +53,7 @@ var tracker = {
   },
 
   onClick: function(event) {
-    console.log(event.target);
+    console.log(event.target.id);
 
     if(event.target.id === 'images') {
       console.log('didn\'t click an image');
@@ -69,104 +66,90 @@ var tracker = {
           allProducts[i].votes++;
         }
       }
-
-
       tracker.displayImages();
+    }
+    if (tracker.clickCount === 25) {
+      var ctx = document.getElementById('myChart').getContext('2d');
+      var optionTypes = {
+        type: 'bar',
+        data: {
+          labels: allProducts.map(function(x) {return x.name;}),
+          datasets: [{
+            label: '# of Votes',
+            data: allProducts.map(function(x) {return x.votes;}),
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+          },
+          ]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
+        }
+      };
+      var myChart = new Chart(ctx, optionTypes);
+      console.log('stuff', myChart);
+      tracker.imagesEl.removeEventListener('click', tracker.onClick);
     }
   }
 };
 
+
 tracker.imagesEl.addEventListener('click', tracker.onClick);
 tracker.displayImages();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var allProducts = [];
-//
-// var productNames = ['bag', 'boots', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb',
-//   'water-can', 'wine-glass'];
-//
-// // You just had to make us use this many pictures...
-//
-//
-// function Product(name, path) {
-//   this.name = name,
-//   this.path = path,
-//   this.clicked = 0;
-//   this.viewed = 0;
-//   allProducts.push(this);
-// }
-// // TODO: Build your constructor and necessary properties.
-//
-// for(var x = 0; x < productNames.length; x++) {
-//   new Product(productNames[x], 'img/' + productNames[x] + '.jpg');
-// }
-//
-// console.log('product names:', productNames);
-//
-//
-// // TODO: Don't forget to build your objects. How can you do this withough having to write 14 lines of `new Product(., ., .)`?
-//
-// var productRank = {
-//   // TODO: All the properties of the object! What do you think you need? Try to write one piece at a time and make sure it does what you want before writing a little more.
-//   // NOTE: A-C-P reminder... Make very intentional and iterative changes to your code, and then A-C-P.
-//
-//   getRandomIndex: function(max) {
-//     return Math.round(Math.random() * (max - 0)) + 0;
-//   },
-//
-//   displayImages: function() {
-//     var picture =  document.getElementsByClassName('threeImg');
-//     var duplicate = [-1, -1, -1];
-//
-//     while(duplicate[0] === duplicate[1] || duplicate[0] === duplicate[2] || duplicate[1] === duplicate[2]) {
-//       duplicate = [productRank.getRandomIndex(allProducts.length), productRank.getRandomIndex(allProducts.length), productRank.getRandomIndex(allProducts.length)];
-//     }
-//
-//     for(var i = 0; i < 3; i++) {
-//       picture[i].src = allProducts[duplicate[i]].path;
-//       allProducts[duplicate[i]].timesShown++;
-//     }
-//   },
-//
-//   tallyClicks: function(elementId) {
-//     // TODO: Hmm... what's going to happen here?
-//   },
-//
-//   displayResults: function() {
-//     // TODO: Hmm... what's going to happen here?
-//   },
-//
-//   showButton: function() {
-//     // TODO: Hmm... what's going to happen here?
-//   },
-//
-//   onClick: function() {
-//     // TODO: Hmm... what's going to happen here?
-//   },
-// };
-//
-// // productRank.imageEls.addEventListener('click', productRank.onClick);
-// productRank.displayImages();
